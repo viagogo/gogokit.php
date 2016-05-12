d2d5f9e2ebdf75379041a89d5cd34ea5069727e2
Maxime Nanouche <maxime.nanouche@gmail.com>
2016-05-12 11:05:53 +0100

    Update token url

--- src/ViagogoConfiguration.php
+++ src/ViagogoConfiguration.php
@@ -6,7 +6,7 @@ namespace Viagogo;
  *
  */
 class ViagogoConfiguration {
-	public static $tokenUrl = 'https://www.viagogo.com/secure/oauth2/token';
+	public static $tokenUrl = 'https://account.viagogo.com/oauth2/token';
 	public static $rootUrl = 'https://api.viagogo.net/v2/';
 	public static $country;
 	public static $language;
